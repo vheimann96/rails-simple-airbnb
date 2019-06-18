8a3fab40481ae1ac198c09a82286507bcd3a3a29
Valentina Heimann <vheimann2@googlemail.com>
2019-06-18 18:11:08 +0300

    Completed show flats rails, missing some frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+10.times do
+  flat = Flat.create!(
+      name: Faker::Name.name,
+      address: Faker::Address.city,
+      description: Faker::Movies::BackToTheFuture.quote,
+      price_per_night: Faker::Number.number(3),
+      number_of_guests: Faker::Number.number(1)
+    )
+end
+
+puts 'Finished!'
